ab5a4895e4d68f2d6a2e6a40ea79cbce6013ac87
Anuj Sharma <anujsharma92_8@live.com>
2022-03-10 19:14:41 +0100

    ball falling on hand and not crossing by fixed

--- Assets/script/motionWP.cs
+++ Assets/script/motionWP.cs
@@ -9,22 +9,37 @@ public class motionWP : MonoBehaviour
     {
         GameObject sphere = GameObject.Find("Sphere");
         sphere.GetComponent<Rigidbody>().useGravity = false;
-       
+        GameObject sensation = GameObject.Find("Sensation");
+        sensation.SetActive(false);
+
 
     }
-
+    bool fig = true;
     // Update is called once per frame
     void Update()
     {
         GameObject sphere = GameObject.Find("Sphere");
-        
+
 
         GameObject leftHand = GameObject.Find("LoPoly Rigged Hand Left");
-        if (leftHand != null && leftHand.activeSelf)
+
+        /*if left hand is null and left hand is active and sphere gravity is disabled then enable gravity*/
+        if (leftHand != null && leftHand.activeSelf && !sphere.GetComponent<Rigidbody>().useGravity)
         {
             sphere.GetComponent<Rigidbody>().useGravity = true;
         }
 
     }
-    
+
+    void OnCollisionEnter(Collision collision)
+    {
+        //Check for a match with the specified name on any GameObject that collides with your GameObject
+        if (collision.gameObject.name == "Contact Fingerbone" || collision.gameObject.name == "Contact Palm Bone") {
+            GameObject sensation = GameObject.Find("Sensation");
+            
+            sensation.SetActive(true);
+
+        }
+    }
+
 }
